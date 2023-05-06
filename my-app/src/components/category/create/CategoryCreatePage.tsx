6ff988b8f6e6f4fd0646b68a5cdade1d9b976a2e
novakvova <novakvova@gmail>
2023-05-06 15:02:11 +0300

    Working form in Formik

--- my-app/src/components/category/create/CategoryCreatePage.tsx
+++ my-app/src/components/category/create/CategoryCreatePage.tsx
@@ -1,20 +1,57 @@
+import { useFormik } from "formik";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { ICategoryCreate } from "./types";
+
 const CategoryCreatePage = () => {
+
+  const initValues : ICategoryCreate = {
+    name: "",
+    image: "",
+    description: "",
+  };
+  
+
+  const onSubmitFormikData = (values: ICategoryCreate) => {
+    console.log("Formik send data", values);
+  }
+
+  const formik = useFormik({
+    initialValues: initValues,
+    onSubmit: onSubmitFormikData
+  });
+
+  const {values, handleSubmit, handleChange} = formik;
+
   return (
     <>
       <h1 className="text-center">Додати категорію</h1>
-      <form className="col-md-6 offset-md-3">
+      <form className="col-md-6 offset-md-3" onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="name" className="form-label">
             Назва
           </label>
-          <input type="text" className="form-control" id="name" name="name" />
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            name="name"
+            value={values.name}
+            onChange={handleChange}
+          />
         </div>
 
         <div className="mb-3">
           <label htmlFor="image" className="form-label">
             Фото
           </label>
-          <input type="text" className="form-control" id="image" name="image" />
+          <input
+            type="text"
+            className="form-control"
+            id="image"
+            name="image"
+            value={values.image}
+            onChange={handleChange}
+          />
         </div>
 
         <div className="form-floating mb-3">
@@ -22,7 +59,11 @@ const CategoryCreatePage = () => {
             className="form-control"
             placeholder="Вкажіть опис"
             id="description"
+            name="description"
             style={{ height: "100px" }}
+            value={values.description}
+            onChange={handleChange}
+
           ></textarea>
           <label htmlFor="description">Опис</label>
         </div>
