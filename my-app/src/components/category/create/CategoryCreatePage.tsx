664a26b79b95a4b2c7487f10e2b6565148b760c7
novakvova <novakvova@gmail>
2023-05-06 16:03:10 +0300

    Working create validator

--- my-app/src/components/category/create/CategoryCreatePage.tsx
+++ my-app/src/components/category/create/CategoryCreatePage.tsx
@@ -1,26 +1,43 @@
 import { useFormik } from "formik";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { ICategoryCreate } from "./types";
+import * as yup  from "yup";
+import classNames from "classnames";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const CategoryCreatePage = () => {
+
+  const navigator = useNavigate();
 
   const initValues : ICategoryCreate = {
     name: "",
     image: "",
     description: "",
   };
+
+  const createSchema=yup.object({
+    name: yup.string().required("Вкажіть назву"),
+    description: yup.string().required("Вкажіть опис"),
+  });
   
 
   const onSubmitFormikData = (values: ICategoryCreate) => {
-    console.log("Formik send data", values);
+    //console.log("Formik send data", values);
+    axios.post("http://127.0.0.1:8000/api/category", values)
+      .then(resp => {
+        //console.log("Create date in server", resp);
+        navigator("/");
+      });
   }
 
   const formik = useFormik({
     initialValues: initValues,
+    validationSchema: createSchema,
     onSubmit: onSubmitFormikData
   });
 
-  const {values, handleSubmit, handleChange} = formik;
+  const {values, errors, touched, handleSubmit, handleChange} = formik;
 
   return (
     <>
@@ -32,12 +49,14 @@ const CategoryCreatePage = () => {
           </label>
           <input
             type="text"
-            className="form-control"
+            className={classNames("form-control", {"is-invalid": errors.name && touched.name})}
             id="name"
             name="name"
             value={values.name}
             onChange={handleChange}
           />
+          {errors.name && touched.name && <div className="invalid-feedback">{errors.name}</div>}
+          
         </div>
 
         <div className="mb-3">
@@ -56,15 +75,15 @@ const CategoryCreatePage = () => {
 
         <div className="form-floating mb-3">
           <textarea
-            className="form-control"
+            className={classNames("form-control", {"is-invalid": errors.description && touched.description})}
             placeholder="Вкажіть опис"
             id="description"
             name="description"
             style={{ height: "100px" }}
             value={values.description}
             onChange={handleChange}
-
           ></textarea>
+          {errors.description && touched.description && <div className="invalid-feedback">{errors.description}</div>}
           <label htmlFor="description">Опис</label>
         </div>
 
