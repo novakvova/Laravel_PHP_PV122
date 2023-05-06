78e7175d24b69cd6c21a44686eaa879e84911eae
novakvova <novakvova@gmail>
2023-05-06 17:50:20 +0300

    Begin woking pagination

--- my-app/src/components/home/HomePage.tsx
+++ my-app/src/components/home/HomePage.tsx
@@ -1,19 +1,23 @@
 import axios from "axios";
+import classNames from "classnames";
 import { useEffect, useState } from "react";
 import { Link, useSearchParams } from "react-router-dom";
 import { ICategoryItem, ICategoryResponse, ICategorySearch } from "./types";
 
 const HomePage = () => {
-
   const [searchParams, setSearchParams] = useSearchParams();
   console.log("page = ", searchParams.get("page"));
-  
-  
+
   const [search, setSearch] = useState<ICategorySearch>({
     page: searchParams.get("page") || 1,
   });
 
-  const [list, setList] = useState<ICategoryItem[]>([]);
+  const [category, setCategory] = useState<ICategoryResponse>({
+    data: [],
+    total: 0,
+    current_page: 0,
+    last_page: 0,
+  });
 
   useEffect(() => {
     axios
@@ -21,15 +25,33 @@ const HomePage = () => {
         params: search,
       })
       .then((resp) => {
-        setList(resp.data.data);
+        //setList(resp.data.data);
+        setCategory(resp.data);
       })
       .catch((bad) => {
         console.log("Bad request", bad);
       });
   }, [search]);
-  
 
-  const dataView = list.map((category) => (
+  const { data, last_page, current_page, total } = category;
+  const buttons = [];
+  for (let i = 1; i <= last_page; i++) {
+    buttons.push(i);
+  }
+
+  const pagination = buttons.map((page) => (
+    <li className={classNames("page-item", {"active": page===current_page})}>
+      <Link
+        className="page-link"
+        to={"?page=" + page}
+        onClick={() => setSearch({...search, page })}
+      >
+        {page}
+      </Link>
+    </li>
+  ));
+
+  const dataView = data.map((category) => (
     <tr key={category.id}>
       <th>
         <img src={category.image} alt="Фотка" width={50} />
@@ -45,19 +67,23 @@ const HomePage = () => {
       <Link className="btn btn-success" to="/categories/create">
         Додати
       </Link>
-      {list.length === 0 ? (
+      <h4>Усього записів: {total}</h4>
+      {data.length === 0 ? (
         <h2>Дані відсутні</h2>
       ) : (
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Фото</th>
-              <th scope="col">Назва</th>
-              <th scope="col">Опис</th>
-            </tr>
-          </thead>
-          <tbody>{dataView}</tbody>
-        </table>
+        <>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Фото</th>
+                <th scope="col">Назва</th>
+                <th scope="col">Опис</th>
+              </tr>
+            </thead>
+            <tbody>{dataView}</tbody>
+          </table>
+            <ul className="pagination justify-content-center">{pagination}</ul>
+        </>
       )}
     </>
   );
