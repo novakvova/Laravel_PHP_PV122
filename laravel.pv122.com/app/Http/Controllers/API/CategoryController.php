5a4aa50bd76ca754115748de717eedfbec92cc4b
novakvova <novakvova@gmail>
2023-06-17 16:01:59 +0300

    Begin working app

--- laravel.pv122.com/app/Http/Controllers/API/CategoryController.php
+++ laravel.pv122.com/app/Http/Controllers/API/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Helpers\ImageWorker;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -52,49 +53,7 @@ class CategoryController extends Controller
         return response()->json($list,200);
     }
 
-    function image_resize($width, $height, $path, $inputName)
-    {
-        list($w,$h)=getimagesize($_FILES[$inputName]['tmp_name']);
-        $maxSize=0;
-        if(($w>$h)and ($width>$height))
-            $maxSize=$width;
-        else
-            $maxSize=$height;
-        $width=$maxSize;
-        $height=$maxSize;
-        $ration_orig=$w/$h;
-        if(1>$ration_orig)
-            $width=ceil($height*$ration_orig);
-        else
-            $height=ceil($width/$ration_orig);
-        //отримуємо файл
-        $imgString=file_get_contents($_FILES[$inputName]['tmp_name']);
-        $image=imagecreatefromstring($imgString);
-        //нове зображення
-        $tmp=imagecreatetruecolor($width,$height);
-        imagecopyresampled($tmp, $image,
-            0,0,
-            0,0,
-            $width, $height,
-            $w, $h);
-        //Зберегти зображення у файлову систему
-        switch($_FILES[$inputName]['type'])
-        {
-            case 'image/jpeg':
-                imagejpeg($tmp,$path,30);
-                break;
-            case 'image/png':
-                imagepng($tmp,$path,0);
-                break;
-            case 'image/gif':
-                imagegif($tmp, $path);
-                break;
-        }
-        return $path;
-        //очисчаємо память
-        imagedestroy($image);
-        imagedestroy($tmp);
-    }
+
 
     /**
      * @OA\Post(
@@ -150,7 +109,8 @@ class CategoryController extends Controller
 
         $sizes = [50, 150, 300, 600, 1200];
         foreach ($sizes as $size) {
-            $this->image_resize($size,$size, $fileSave.$size.'_'.$filename, 'image');
+            ImageWorker::image_resize($size,$size, $fileSave.$size.'_'.$filename, 'image');
+            //$this->image_resize($size,$size, $fileSave.$size.'_'.$filename, 'image');
         }
         $input["image"] = $filename;
         $category = Category::create($input);
